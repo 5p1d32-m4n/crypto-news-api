8c744ed74dd884cc68487bb2c7d586a0bd67f203
Brandon J. Baker Escarraman <brandon.j.baker1011@gmail.com>
2022-04-22 00:50:09 -0400

    Added first axios call to news site and git ignore

--- index.js
+++ index.js
@@ -9,4 +9,12 @@ app.get('/', (req , res)=>{
 	res.json('Welcome to my Crypto News API.')
 })
 
+app.get('/news', (req, res)=>{
+	axios.get('https://cointelegraph.com/')
+		.then((response)=>{
+			const html = response.data
+			console.log(html)
+		})
+})
+
 app.listen(PORT, () => console.log(`server running on PORT: ${PORT}`))
